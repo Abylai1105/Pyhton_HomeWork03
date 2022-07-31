8c659fc7f9983b561ac94018c6aec4758da066be
Abylai <aba.omar@mail.ru>
2022-07-31 21:10:09 +0600

    HW04_Task02

--- Seminar04/HomeWork04/Task02.py
+++ Seminar04/HomeWork04/Task02.py
@@ -2,3 +2,15 @@
 # которая выведет список неповторяющихся элементов исходной последовательности. 
 # Посмотрели, что такое множество? Вот здесь его не используйте.
 
+subsequence  = [4, 5, 6, 4, 5, 6, 4, 5, 6]
+
+
+def sort_subsequence(sub: list):
+    sub2 = sorted(sub)
+    n = []
+    for i in sub2:
+        if i not in n:
+            n.append(i)
+    return n
+
+print(sort_subsequence(subsequence))
